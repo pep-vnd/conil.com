ab50a37d6f2043b1937767b0b22d65718bceba53
xKatzet <pepecarmona2904@gmail.com>
2026-02-03 19:01:18 +0100

    Arreglo aplication error

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,5 +47,21 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/gestionar/:path*',
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public files
+     */
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
 };
